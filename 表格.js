7031de1b21191e47b4ce2b228bb627728007fe82
wang.xin <wang.xin>
2024-06-24 18:16:52 +0800

    feat: 2015

---
+++ 表格.js
@@ -0,0 +1,36 @@
+(function () {
+
+    // 假设我们要抓取的第一个表格就是目标表格
+    var table = document.querySelector('.article table');
+
+    // 存储表格数据
+    var data = [];
+
+    // 遍历表格的行
+    for (var i = 1; i < table.rows.length; i++) { // 从1开始，跳过表头
+        var row = table.rows[i];
+        var rowData = [];
+        // 遍历单元格
+        for (var j = 0; j < row.cells.length; j++) {
+            rowData.push(row.cells[j].textContent || row.cells[j].innerText);
+        }
+        // 将行数据添加到数据数组
+        data.push(rowData);
+    }
+
+    // 将数据转换为CSV格式
+    var csvContent = "data:text/csv;charset=utf-8,";
+    data.forEach(function (rowArray) {
+        var row = rowArray.join(",");
+        csvContent += row + "\r\n";
+    });
+
+    // 创建虚拟链接用于下载
+    var encodedUri = encodeURI(csvContent);
+    var link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "table_data.csv"); // 设置下载文件名
+
+    // 模拟点击链接进行下载
+    link.click();
+})();
