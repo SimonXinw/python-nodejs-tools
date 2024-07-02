8d5ab91f3af092c1636428748e78b2cf0a53c956
wang.xin <wang.xin>
2024-07-02 13:10:49 +0800

    feat: 提交广东北京文件

--- parse.py
+++ parse.py
@@ -1,15 +1,15 @@
 import os
 import shutil
 
-def copy_and_overwrite_excel_files_in_subfolders(base_folder_path):
+def copy_and_overwrite_files_in_subfolders(base_folder_path):
     # 遍历当前文件夹下的所有子文件夹
     for root, dirs, files in os.walk(base_folder_path):
         for file in files:
-            if file.endswith(".xlsx") or file.endswith(".xls"):
+            if file.endswith((".xlsx", ".xls", ".pdf")):
                 file_path = os.path.join(root, file)
                 temp_file_path = os.path.join(root, "temp_" + file)
                 
-                # 复制文件到临时 文件
+                # 复制文件到临时文件
                 try:
                     shutil.copy(file_path, temp_file_path)
                     print(f"已创建临时文件: {temp_file_path}")
@@ -22,4 +22,4 @@ def copy_and_overwrite_excel_files_in_subfolders(base_folder_path):
 
 # 示例用法
 base_folder_path = '.'
-copy_and_overwrite_excel_files_in_subfolders(base_folder_path)
+copy_and_overwrite_files_in_subfolders(base_folder_path)
